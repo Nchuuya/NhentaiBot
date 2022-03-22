abce333b1b65001d8d79a77f5f47efbe7fae4b30
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-22 19:05:41 +0530

    Update starter.py

--- API/starter.py
+++ API/starter.py
@@ -5,7 +5,7 @@ from telethon import events
 
 class start():
 
-    @bot.on(events.NewMessage(pattern=r"^/start$|^/start@Nhentaireaderbot"))
+    @bot.on(events.NewMessage(pattern=r"^/nhstart$|^/start@Nhentaireaderbot"))
     async def event_handler_start(event):
         await bot.send_message(
             event.chat_id,
@@ -13,14 +13,14 @@ class start():
             file='https://tenor.com/view/dancing-anime-sexy-hentai-gif-5476864'
         )
 
-    @bot.on(events.NewMessage(pattern=r"^/help$|^/help@Nhentaireaderbot"))
+    @bot.on(events.NewMessage(pattern=r"^/nhelp$|^/nhelp@Nhentaireaderbot"))
     async def event_handler_help(event):
         await bot.send_message(
             event.chat_id,
             help_text
             )
 
-    @bot.on(events.NewMessage(pattern="/source"))
+    @bot.on(events.NewMessage(pattern="/nsource"))
     async def event_handler_source(event):
         await bot.send_message(
             event.chat_id,
